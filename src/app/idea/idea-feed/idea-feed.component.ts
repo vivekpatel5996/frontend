d5be1188915a18107ed20e3e50c7d58b44d1165b
vivekpatel5996 <vivekjasubhai4@gmail.com>
2018-11-17 12:17:24 +0530

    Implemented virtual scroll in idea feed using dummy data

--- src/app/idea/idea-feed/idea-feed.component.ts
+++ src/app/idea/idea-feed/idea-feed.component.ts
@@ -6,7 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./idea-feed.component.scss']
 })
 export class IdeaFeedComponent implements OnInit {
+  ideas: { idea_name: string; description: string; tags: string[]; require_assistance: boolean }[] = [];
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    for (let i = 0; i < 10000; i++) {
+      this.ideas.push({
+        idea_name: `idea${i}`,
+        description: `description${i}`,
+        tags: [`tags${i}`, `tags${i}`, `tags${i}`],
+        require_assistance: true
+      });
+    }
+  }
 }
